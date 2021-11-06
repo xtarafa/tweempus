047454153ee8043276c6e99e94ca8b4d632df7f0
Xavi Tarafa <xtarafa@gmail.com>
2021-11-06 17:02:39 +0100

    Tweempus Modulo 3

--- src/app/shared/twimp/twimp-card/twimp-card.component.ts
+++ src/app/shared/twimp/twimp-card/twimp-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Twimp } from '../twimp.model';
 
 @Component({
   selector: 'tweempus-twimp-card',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./twimp-card.component.css']
 })
 export class TwimpCardComponent  {
-
+  @Input() twimp! :Twimp;
 }
--- src/app/shared/twimp/twimp-list/twimp-list.component.ts
+++ src/app/shared/twimp/twimp-list/twimp-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Twimp } from '../twimp.model';
+import { Author } from '../../author/author.model';
+
 @Component({
   selector: 'tweempus-twimp-list',
   templateUrl: './twimp-list.component.html',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TwimpListComponent implements OnInit {
 
-  constructor() { }
+  text: string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam efficitur sodales libero, sit amet posuere arcu consectetur ut. Nam volutpat ligula ac nunc consectetur vestibulum.";
+  authors: Author[] = [];
+  twimps: Twimp[] = [];
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.authors.push(new Author('1'));
+    this.twimps.push(new Twimp('1', '', this.authors[0], this.text,'01/01/2020'));
+    this.twimps.push(new Twimp('2', '', this.authors[0], this.text,'01/01/2020'));
+    this.twimps.push(new Twimp('3', '', this.authors[0], this.text,'01/01/2020'));
+    this.twimps.push(new Twimp('4', '', this.authors[0], this.text,'01/01/2020'));
+    this.twimps.push(new Twimp('5', '', this.authors[0], this.text,'01/01/2020'));
   }
-
 }
